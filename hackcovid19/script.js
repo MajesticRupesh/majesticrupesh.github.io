e4da5e37964f54cbaeae9f0bce6130e85e07553e
Rupesh Pal <rupcomp1@gmail.com>
2020-04-17 23:55:32 +0530

    fix

--- hackcovid19/script.js
+++ hackcovid19/script.js
@@ -1,5 +1,7 @@
 function load_data() {
-    $.getJSON('data.json', function(data) {         
-        console.log(data.responseText);
-    });
+    var request = new XMLHttpRequest();
+    request.open("GET", "data.json", false);
+    request.send(null)
+    var my_JSON_object = JSON.parse(request.responseText);
+    console.log(my_JSON_object);
 }
