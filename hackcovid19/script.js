909c01bbfb838af2039b7acc466d6454f66720d8
Rupesh Pal <rupcomp1@gmail.com>
2020-04-18 00:24:57 +0530

    test

--- hackcovid19/script.js
+++ hackcovid19/script.js
@@ -4,4 +4,5 @@ function load_data() {
     request.send(null)
     var my_JSON_object = JSON.parse(request.responseText);
     console.log(my_JSON_object);
+    console.log(my_JSON_object.age.question);
 }
